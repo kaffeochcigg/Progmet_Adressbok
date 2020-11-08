9c40d21dea1eb426e29b52533afdecb3928e5af1
Oscar Carlsson <Oscar.carlsson@LIVE.COM>
2020-11-08 21:05:10 +0100

    Finished release

--- Program.cs
+++ Program.cs
@@ -11,14 +11,14 @@ namespace Progmet_Adressbok
     {
         class Person
         {
-            public string newname, newadress, newphone, newemail;
+            public string name, adress, phone, email;
 
             public Person(string name, string adress, string phone, string email)
             {
-                newname = name;
-                newadress = adress;
-                newphone = phone;
-                newemail = email;
+                this.name = name;
+                this.adress = adress;
+                this.phone = phone;
+                this.email = email;
             }
         }
         static void Main(string[] args)
@@ -26,45 +26,44 @@ namespace Progmet_Adressbok
             string fileName = @"C:\Users\Oscar\bin\adressbok.txt"; //Change to appropriate path for yourself
             List<Person> people = new List<Person>();
             bool quit = false;
-            string[] infobook = File.ReadAllLines(fileName);
-            string[] info = new string[4];
+            string[] infobook = File.ReadAllLines(fileName); //found@https://www.c-sharpcorner.com/UploadFile/mahesh/how-to-read-a-text-file-in-C-Sharp/          
             for (int i = 0; i < infobook.Length; i++)
             {
-                info = infobook[i].Split('#');
+                string[] info = infobook[i].Split('#');
                 people.Add(new Person(info[0], info[1], info[2], info[3]));
             }
 
             Menu();
+            
             while (!quit)
-            {               
+            {
                 Console.Write(">");
-                string input = Console.ReadLine();
+                string input = Console.ReadLine().ToLower();
                 if (input == "avsluta")
                 {
+                    Console.Clear();
+                    Console.WriteLine("Välkommen åter!");
                     return;
                 }
                 else if (input == "visa")
                 {
-                    
-                   
-                    Console.WriteLine("{0,-18}{1,-23}{2,-16}{3}", "Namn", "Adress", "Telefon", "Email");
+                    Console.WriteLine("\n{0,-18}{1,-23}{2,-16}{3}", "Namn", "Adress", "Telefon", "Email");
                     Console.WriteLine("*************************************************************************************************");
-                       
+
                     for (int i = 0; i < people.Count; i++)
                     {
                         if (people[i] != null)
                         {
-                            
+
                             Console.WriteLine("{0,-17} {1,-22} {2,-15} {3}",
-                                people[i].newname, people[i].newadress, people[i].newphone, people[i].newemail);
-                            
+                                people[i].name, people[i].adress, people[i].phone, people[i].email);
+
                         }
                     }
                     Console.WriteLine("*************************************************************************************************");
                 }
                 else if (input == "ny")
                 {
-
                     Console.Write("Ange namn: ");
                     string name = Console.ReadLine();
                     Console.Write("Ange adress: ");
@@ -76,15 +75,14 @@ namespace Progmet_Adressbok
                     people.Add(new Person(name, address, phone, mail));
                     Console.WriteLine("Ny kontakt tillagd");
                     //Saving right away when adding new
-                    using (StreamWriter writerDoc = new StreamWriter(fileName))
+                    using (StreamWriter sw = new StreamWriter(fileName))
                     {
                         for (int i = 0; i < people.Count(); i++)
                         {
-                            writerDoc.WriteLine("{0}#{1}#{2}#{3}", people[i].newname, people[i].newadress, people[i].newphone, people[i].newemail);
+                            sw.WriteLine("{0}#{1}#{2}#{3}", people[i].name, people[i].adress, people[i].phone, people[i].email);
 
                         }
                     }
-
                 }
                 else if (input == "radera")
                 {
@@ -92,24 +90,27 @@ namespace Progmet_Adressbok
                     string name = Console.ReadLine().ToLower();
                     for (int i = 0; i < people.Count; i++)
                     {
-                        if (name == people[i].newname.ToLower())
+                        if (name == people[i].name.ToLower())
                         {
                             Console.WriteLine("Tog bort {0} från dina kontakter", name);
                             people.RemoveAt(i);
                         }
-                        
+
                     }
                     //Deleting right away
-                    using (StreamWriter writerDoc = new StreamWriter(fileName))
+                    using (StreamWriter sw = new StreamWriter(fileName))
                     {
                         for (int i = 0; i < people.Count(); i++)
                         {
-                            writerDoc.WriteLine("{0}#{1}#{2}#{3}", people[i].newname, people[i].newadress, people[i].newphone, people[i].newemail);
+                            sw.WriteLine("{0}#{1}#{2}#{3}", people[i].name, people[i].adress, people[i].phone, people[i].email);
 
                         }
                     }
                 }
-
+                else
+                {
+                    Console.WriteLine($"Ogiltigt kommando: {input}");
+                }
             }
         }
         static void Menu()
@@ -122,6 +123,8 @@ namespace Progmet_Adressbok
             Console.WriteLine(" 'ny' för at skapa en ny kontakt      ");
             Console.WriteLine(" 'radera' för at ta bort en kontakt   ");
             Console.WriteLine(" 'avsluta' för att stänga programmet  ");
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("    Kontakter sparas automatiskt!     ");
             Console.WriteLine("--------------------------------------");
         }
     }
