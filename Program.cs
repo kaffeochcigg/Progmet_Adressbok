f5cc18f517671cf333cba6ccdb1d86c60d24ee9e
Oscar Carlsson <Oscar.carlsson@LIVE.COM>
2020-11-08 17:53:54 +0100

    städa

--- Program.cs
+++ Program.cs
@@ -23,43 +23,20 @@ namespace Progmet_Adressbok
         }
         static void Main(string[] args)
         {
-
-            string fileName = @"C:\Users\Oscar\bin\adressbok.txt";
+            string fileName = @"C:\Users\Oscar\bin\adressbok.txt"; //Change to appropriate path for yourself
             List<Person> people = new List<Person>();
             bool quit = false;
-            string[] telbok = File.ReadAllLines(fileName);
-            string[] k = new string[4];
-            for (int i = 0; i < telbok.Length; i++)
+            string[] infobook = File.ReadAllLines(fileName);
+            string[] info = new string[4];
+            for (int i = 0; i < infobook.Length; i++)
             {
-                k = telbok[i].Split('#');
-                people.Add(new Person(k[0], k[1], k[2], k[3]));
+                info = infobook[i].Split('#');
+                people.Add(new Person(info[0], info[1], info[2], info[3]));
             }
-            /*using (StreamReader file = new StreamReader(fileName))
 
-            {
-
-                //Test
-                while ((fileName = file.ReadLine()) != null)
-                {
-                    string[] info = fileName.Split('#');
-                    // Console.WriteLine(line);
-                    // Console.WriteLine("{0} - {1}", words[0], words[1]);
-                    people.Add(new Person(info[0], info[1], info[2], info[3]));
-                }
-                file.Close();
-
-            }*/
-            /*for (int i = 0; i < people.Count; i++)
-            {
-                if (people[i] != null)
-                {
-                    Console.WriteLine("{0}, {1}, {2}, {3}",
-                        people[i].newname, people[i].newadress, people[i].newphone, people[i].newemail);
-                }
-            }*/
             Menu();
             while (!quit)
-            {
+            {               
                 Console.Write(">");
                 string input = Console.ReadLine();
                 if (input == "avsluta")
@@ -68,7 +45,9 @@ namespace Progmet_Adressbok
                 }
                 else if (input == "visa")
                 {
-                    Console.WriteLine("{0,-17}{1,-23}{2,-14}{3}", "Namn", "Adress", "Telefon", "Email");
+                    
+                   
+                    Console.WriteLine("{0,-18}{1,-23}{2,-16}{3}", "Namn", "Adress", "Telefon", "Email");
                     Console.WriteLine("*************************************************************************************************");
                        
                     for (int i = 0; i < people.Count; i++)
@@ -76,7 +55,7 @@ namespace Progmet_Adressbok
                         if (people[i] != null)
                         {
                             
-                            Console.WriteLine("{0,-15} {1,-22} {2,-13} {3}",
+                            Console.WriteLine("{0,-17} {1,-22} {2,-15} {3}",
                                 people[i].newname, people[i].newadress, people[i].newphone, people[i].newemail);
                             
                         }
@@ -86,16 +65,17 @@ namespace Progmet_Adressbok
                 else if (input == "ny")
                 {
 
-                    Console.Write("New contact name: ");
+                    Console.Write("Ange namn: ");
                     string name = Console.ReadLine();
-                    Console.Write("The address: ");
+                    Console.Write("Ange adress: ");
                     string address = Console.ReadLine();
-                    Console.Write("The Phone number: ");
+                    Console.Write("Ange telefonnummer: ");
                     string phone = Console.ReadLine();
-                    Console.Write("The E-mail: ");
+                    Console.Write("Ange E-mail: ");
                     string mail = Console.ReadLine();
                     people.Add(new Person(name, address, phone, mail));
-                    Console.WriteLine("New contact added!");
+                    Console.WriteLine("Ny kontakt tillagd");
+                    //Saving right away when adding new
                     using (StreamWriter writerDoc = new StreamWriter(fileName))
                     {
                         for (int i = 0; i < people.Count(); i++)
@@ -119,6 +99,7 @@ namespace Progmet_Adressbok
                         }
                         
                     }
+                    //Deleting right away
                     using (StreamWriter writerDoc = new StreamWriter(fileName))
                     {
                         for (int i = 0; i < people.Count(); i++)
